51df7a8eea73e833db876db24577669d4a4dba4b
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-02-06 19:25:46 +0900

    changebuttonmargin

--- app/components/GenreButtons.jsx
+++ app/components/GenreButtons.jsx
@@ -12,7 +12,7 @@ const GenreButtons = ({ id, name, changeGenre, genreId }) => {
       <button
         onClick={handleClick}
         value={id}
-        className="px-6 py-2 border-2 border-gray-200  
+        className="px-6 py-2 border-2 mt-2 border-gray-200  
                     text-gray-900 font-medium text-xs leading-tight rounded bg-gray-300
                     hover:bg-gray-50 hover:bg-opacity-70 focus:outline-none focus:ring-1 
                     focus:bg-lime-100 transition duration-150 ease-in-out"
